4b5d61a4efd03f9a25c10e5d2f0b92117586c886
DeenankSharma <sharmadeenank@gmail.com>
2024-05-24 19:36:24 +0530

    first-project

--- backend/3.5 Secrets Project/index.js
+++ backend/3.5 Secrets Project/index.js
@@ -1,3 +1,43 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+var isvalid=false;
+const app = express();
+const port = 3000;
+
+const _dirname = dirname(fileURLToPath(import.meta.url));
+app.use(bodyParser.urlencoded({extended:true}));
+
+function passwordCheck(req,res,next){
+    if(req.body["password"] === "satoshi"){
+        isvalid = true;
+    }
+    next();
+}
+
+app.use(passwordCheck);
+
+app.get("/",(req,res)=>{
+    res.sendFile(_dirname + "/public/index.html");
+})
+
+
+
+app.post("/check",(req,res)=>{
+    if(isvalid){
+        res.sendFile(_dirname + "/public/secret.html");
+    }
+    else{
+        res.sendFile(_dirname + "/public/index.html");
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`listening on ${port}`);
+})
