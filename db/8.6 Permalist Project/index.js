5c5752430842ce6e6f3a549bbacf5da9a7c5a9ef
DeenankSharma <sharmadeenank@gmail.com>
2024-06-10 21:09:06 +0530

    Permalist Project

---
+++ db/8.6 Permalist Project/index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from 'pg'
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "todo",
+  password: "new_password",
+  port: 5432,
+})
+
+db.connect()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [];
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM todo_items;")
+  items = result.rows
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: items,
+  });
+});
+
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+  await db.query("INSERT INTO todo_items (title) VALUES ($1);",[item])
+  res.redirect("/");
+});
+
+app.post("/edit", async (req, res) => {
+  const id = req.body.updatedItemId
+  const title = req.body.updatedItemTitle
+  await db.query("UPDATE todo_items SET title = $1 WHERE id = $2;",[title,id])
+  res.redirect("/")
+});
+
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId
+  await db.query("DELETE FROM todo_items WHERE id = $1;",[id]);
+  res.redirect("/")
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
